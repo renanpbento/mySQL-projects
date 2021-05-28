fe32cbe4e39795fedce8bd7e4c58c99868e80db4
Renan Pereira Bento <38670752+renanpbento@users.noreply.github.com>
2021-05-27 21:57:45 -0300

    Update Maven Analytics - Advanced SQL 4.25 - 4.31.sql

--- Maven Analytics - Advanced SQL 4.25 - 4.31.sql
+++ Maven Analytics - Advanced SQL 4.25 - 4.31.sql
@@ -8,9 +8,9 @@ USE mavenfuzzyfactory;
 
 -- Example: Bid Optimization & Trend Analysis
 SELECT
-	primary_product_id,
+    primary_product_id,
     COUNT(DISTINCT CASE WHEN items_purchased = 1 THEN order_id ELSE NULL END) AS count_single_item_orders,
-	COUNT(DISTINCT CASE WHEN items_purchased = 2 THEN order_id ELSE NULL END) AS count_two_item_orders
+    COUNT(DISTINCT CASE WHEN items_purchased = 2 THEN order_id ELSE NULL END) AS count_two_item_orders
 FROM orders
 WHERE order_id BETWEEN 31000 AND 32000
 GROUP BY 1;
@@ -18,44 +18,44 @@ GROUP BY 1;
 -- (1) Assignment: Analyzing the weekly effect of a bid down in gsearch nonbrand volume
 SELECT
     MIN(DATE(created_at)) AS week_started_at,
-	COUNT(DISTINCT website_session_id) AS sessions
+    COUNT(DISTINCT website_session_id) AS sessions
 FROM website_sessions
 WHERE
-	utm_source = 'gsearch'
+    utm_source = 'gsearch'
     AND utm_campaign = 'nonbrand'
     AND created_at < '2012-05-10'
 GROUP BY
-	YEAR(created_at),
+    YEAR(created_at),
     WEEK(created_at);
 
 -- (2) Pulling conversion rates from sessions to order by device type
 SELECT
-	device_type,
-	COUNT(DISTINCT order_id) AS orders,
+    device_type,
+    COUNT(DISTINCT order_id) AS orders,
     COUNT(DISTINCT website_sessions.website_session_id) AS sessions,
-	COUNT(DISTINCT order_id)/COUNT(DISTINCT website_sessions.website_session_id) AS cvr
+    COUNT(DISTINCT order_id)/COUNT(DISTINCT website_sessions.website_session_id) AS cvr
 FROM website_sessions
 LEFT JOIN orders ON website_sessions.website_session_id = orders.website_session_id
 WHERE
-	website_sessions.created_at < '2012-05-11'
+    website_sessions.created_at < '2012-05-11'
     AND utm_source = 'gsearch'
     AND utm_campaign = 'nonbrand'
 GROUP BY
-	device_type;
+    device_type;
 
 -- (3) Analyzing impact on volume of a bid through weekly trends for desktop and mobile
 SELECT
-	MIN(DATE(created_at)) AS week_start_date,
-	COUNT(DISTINCT CASE WHEN device_type = 'desktop' THEN website_session_id ELSE NULL END) AS desktop_sessions,
-	COUNT(DISTINCT CASE WHEN device_type = 'mobile' THEN website_session_id ELSE NULL END) AS mobile_sessions
+    MIN(DATE(created_at)) AS week_start_date,
+    COUNT(DISTINCT CASE WHEN device_type = 'desktop' THEN website_session_id ELSE NULL END) AS desktop_sessions,
+    COUNT(DISTINCT CASE WHEN device_type = 'mobile' THEN website_session_id ELSE NULL END) AS mobile_sessions
 FROM website_sessions
 WHERE
-	created_at < '2012-06-09'
-	AND created_at > '2012-04-15'
+    created_at < '2012-06-09'
+    AND created_at > '2012-04-15'
     AND utm_source = 'gsearch'
     AND utm_campaign = 'nonbrand'
 GROUP BY
-	YEAR(created_at),
+    YEAR(created_at),
     WEEK(created_at);
     
 
