6fa354f6cb0fe69686b2fd6e88ba4e5a79183463
Renan Pereira Bento <38670752+renanpbento@users.noreply.github.com>
2021-05-30 09:26:23 -0300

    Update Maven Analytics - Advanced SQL 5.32 - 5.37.sql

--- Maven Analytics - Advanced SQL 5.32 - 5.37.sql
+++ Maven Analytics - Advanced SQL 5.32 - 5.37.sql
@@ -9,69 +9,69 @@ USE mavenfuzzyfactory;
 -- Example: Analyzing Top Website Pages & Entry Pages (Temporary Table Resource)
 CREATE TEMPORARY TABLE first_pageview
 SELECT
-	website_session_id,
+    website_session_id,
     MIN(website_pageview_id) AS min_pv_id
 FROM website_pageviews
 WHERE 
-	website_pageview_id < 1000
+    website_pageview_id < 1000
 GROUP BY
-	website_session_id;
+    website_session_id;
     
 SELECT
-	website_pageviews.pageview_url AS landing_page,
+    website_pageviews.pageview_url AS landing_page,
     COUNT(DISTINCT first_pageview.website_session_id) AS sessions_hitting_this_lander
 FROM first_pageview
-	LEFT JOIN website_pageviews ON first_pageview.min_pv_id = website_pageviews.website_pageview_id
+    LEFT JOIN website_pageviews ON first_pageview.min_pv_id = website_pageviews.website_pageview_id
 GROUP BY
-	website_pageviews.pageview_url;
+    website_pageviews.pageview_url;
 
 -- Assignment: Most-viewed website pages, ranked by session volume (until 2012-06-09, not included)
 
 /* Breaking down the problem
 To answer this question, it will be necessary to pull two sets of data:
-	- Pageview Url (Website pages)
+    - Pageview Url (Website pages)
     - Sessions (Volume)*/
 
 -- First Attempt: it looks like the "website_pageviews" Table has the required data
 SELECT
-	pageview_url AS website_page,
+    pageview_url AS website_page,
     COUNT(DISTINCT website_session_id) AS sessions_volume
 FROM website_pageviews
 WHERE
-	created_at < '2012-06-09'
+    created_at < '2012-06-09'
 GROUP BY
-	website_page
+    website_page
 ORDER BY
-	sessions_volume DESC;
+    sessions_volume DESC;
 -- Spot On Solution!
 
 -- Assignment: Finding top entry pages, ranked by entry volume (until 2012-06-12, not included)
 
 /* Breaking down the problem:
 To answer this question, it will be necessary to pull two sets of data:
-	- Landing Page (Website Pageview Id)
+    - Landing Page (Website Pageview Id)
     - Sessions (Volume) */
 
 -- First Attempt
 -- (1) Creating a Temporary Table of first pageviews (entry session)
 CREATE TEMPORARY TABLE entry_session
 SELECT
-	website_session_id,
+    website_session_id,
     MIN(website_pageview_id) AS first_session
 FROM website_pageviews
 WHERE
-	created_at < '2012-06-12'
+    created_at < '2012-06-12'
 GROUP BY
-	website_session_id;
+    website_session_id;
 
 -- (2) Joining the Temporary Table to the "website_pageviews" to find entry sessions per landing page
 SELECT
-	website_pageviews.pageview_url AS landing_page,
+    website_pageviews.pageview_url AS landing_page,
     COUNT(DISTINCT entry_session.first_session) AS entry_volume
 FROM entry_session
-	LEFT JOIN website_pageviews ON website_pageviews.website_pageview_id = entry_session.first_session
+    LEFT JOIN website_pageviews ON website_pageviews.website_pageview_id = entry_session.first_session
 GROUP BY
-	landing_page
+    landing_page
 ORDER BY
-	entry_volume DESC;
+    entry_volume DESC;
 -- Spot On Solution!
