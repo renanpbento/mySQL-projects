8c1a4ae17c048238c81242b6e34d74d849d9c124
Renan Pereira Bento <38670752+renanpbento@users.noreply.github.com>
2021-06-10 21:24:06 -0300

    Delete 21_06_10_Maven Analytics - Advanced SQL 5.43 - 5.49.sql

--- 21_06_10_Maven Analytics - Advanced SQL 5.43 - 5.49.sql
+++
@@ -1,7 +0,0 @@
--- Maven Analytics - Advanced SQL: MySQL Data Analysis & Business Intelligence
-
-SET GLOBAL max_allowed_packet=16M;
-
-USE mavenfuzzyfactory;
-
--- Section 5: Analyzing Website Performance (Lesson 43 - 49)
