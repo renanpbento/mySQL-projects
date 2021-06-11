dfd9894173fe8eb9a0a46d06b76f443dad0367a8
Renan Pereira Bento <38670752+renanpbento@users.noreply.github.com>
2021-06-10 21:23:43 -0300

    Add files via upload

---
+++ 21_06_10_Maven Analytics - Advanced SQL 5.43 - 5.49.sql
@@ -0,0 +1,7 @@
+-- Maven Analytics - Advanced SQL: MySQL Data Analysis & Business Intelligence
+
+SET GLOBAL max_allowed_packet=16M;
+
+USE mavenfuzzyfactory;
+
+-- Section 5: Analyzing Website Performance (Lesson 43 - 49)
