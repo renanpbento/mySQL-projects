e70d349c0fcc28c89be7c67081d039a4e75defac
Renan Pereira Bento <38670752+renanpbento@users.noreply.github.com>
2021-07-27 20:35:31 -0300

    Update 21_07_27_Maven Analytics - Advanced SQL 7.54 - 7.56.sql

--- 21_07_27_Maven Analytics - Advanced SQL 7.54 - 7.56.sql
+++ 21_07_27_Maven Analytics - Advanced SQL 7.54 - 7.56.sql
@@ -19,19 +19,19 @@ so I can get a sense for how important this will be for the business?*/
 
 
 SELECT
-	MIN(DATE(created_at)) AS week_start_date,
+    MIN(DATE(created_at)) AS week_start_date,
     COUNT(DISTINCT website_session_id) AS total_sessions,
-	COUNT(DISTINCT CASE WHEN utm_source = 'gsearch' THEN website_session_id ELSE NULL END) AS gsearch_sessions,
+    COUNT(DISTINCT CASE WHEN utm_source = 'gsearch' THEN website_session_id ELSE NULL END) AS gsearch_sessions,
     ROUND((COUNT(DISTINCT CASE WHEN utm_source = 'gsearch' THEN website_session_id ELSE NULL END)/COUNT(DISTINCT website_session_id) * 100),2) AS gsearch_share,
     COUNT(DISTINCT CASE WHEN utm_source = 'bsearch' THEN website_session_id ELSE NULL END) AS bsearch_sessions,
     ROUND((COUNT(DISTINCT CASE WHEN utm_source = 'bsearch' THEN website_session_id ELSE NULL END)/COUNT(DISTINCT website_session_id) * 100),2) AS bsearch_share,
     ROUND(COUNT(DISTINCT CASE WHEN utm_source = 'gsearch' THEN website_session_id ELSE NULL END)/COUNT(DISTINCT CASE WHEN utm_source = 'bsearch' THEN website_session_id ELSE NULL END),0) AS gsearch_bsearch_ratio
 FROM website_sessions
 WHERE
-	created_at BETWEEN '2012-08-23' AND '2012-11-28'
+    created_at BETWEEN '2012-08-23' AND '2012-11-28'
     AND utm_campaign = 'nonbrand'
 GROUP BY
-	YEARWEEK(created_at);
+    YEARWEEK(created_at);
 
 /* Analyzing the data, bsearch can be considered a relevant channel as it gets roughly 
 a third of gsearch traffic and approximately 25% of total sessions. There is no sign of
