00ccf65cd9f43de01d5c12fda5c0fc2b125692a0
Renan Pereira Bento <38670752+renanpbento@users.noreply.github.com>
2021-07-29 22:38:03 -0300

    Update 21_07_29_Maven Analytics - Advanced SQL 7.57 - 7.58.sql

--- 21_07_29_Maven Analytics - Advanced SQL 7.57 - 7.58.sql
+++ 21_07_29_Maven Analytics - Advanced SQL 7.57 - 7.58.sql
@@ -11,16 +11,16 @@ USE mavenfuzzyfactory;
 the percentage of traffic coming on Mobile and compare to gsearch? */
 
 SELECT
-	utm_source,
-	COUNT(DISTINCT website_session_id) AS sessions,
+    utm_source,
+    COUNT(DISTINCT website_session_id) AS sessions,
     COUNT(DISTINCT CASE WHEN device_type = 'mobile' THEN website_session_id ELSE NULL END) AS mobile_sessions,
-	ROUND((COUNT(DISTINCT CASE WHEN device_type = 'mobile' THEN website_session_id ELSE NULL END)/
+    ROUND((COUNT(DISTINCT CASE WHEN device_type = 'mobile' THEN website_session_id ELSE NULL END)/
     COUNT(DISTINCT website_session_id) * 100), 2) AS pct_mobile
 FROM website_sessions
 WHERE
-	created_at BETWEEN '2012-08-23' AND '2012-11-29'
+    created_at BETWEEN '2012-08-23' AND '2012-11-29'
     AND utm_campaign = 'nonbrand'
 GROUP BY
-	utm_source;
+    utm_source;
     
 -- The analysis pointed to a significant difference in percentage of mobile sessions between bsearch and gsearch.
