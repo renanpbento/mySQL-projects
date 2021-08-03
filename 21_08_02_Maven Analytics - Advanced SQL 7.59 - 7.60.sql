0306db6350a95741f48948bfc559ab76bf288543
Renan Pereira Bento <38670752+renanpbento@users.noreply.github.com>
2021-08-02 21:26:56 -0300

    Update 21_08_02_Maven Analytics - Advanced SQL 7.59 - 7.60.sql

--- 21_08_02_Maven Analytics - Advanced SQL 7.59 - 7.60.sql
+++ 21_08_02_Maven Analytics - Advanced SQL 7.59 - 7.60.sql
@@ -12,15 +12,15 @@ pull nonbrand conversion rates from session to order for gsearch and bsearch, an
 the data by device type? */
 
 SELECT
-	ws.device_type,
-	ws.utm_source,
+    ws.device_type,
+    ws.utm_source,
     COUNT(DISTINCT ws.website_session_id) AS sessions,
     COUNT(DISTINCT o.order_id) AS orders,
     ROUND((COUNT(DISTINCT o.order_id)/COUNT(DISTINCT ws.website_session_id) * 100), 2) AS conv_rate
 FROM website_sessions ws
 LEFT JOIN orders o ON ws.website_session_id = o.website_session_id
 WHERE
-	ws.created_at BETWEEN '2012-08-22' AND '2012-09-18'
+    ws.created_at BETWEEN '2012-08-22' AND '2012-09-18'
     AND utm_campaign = 'nonbrand'
 GROUP BY
-	1, 2
+    1, 2
