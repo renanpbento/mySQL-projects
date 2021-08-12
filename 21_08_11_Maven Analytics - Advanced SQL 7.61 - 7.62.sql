94bd669932f89f9eb8975c071d9de2dbab7a820b
Renan Pereira Bento <38670752+renanpbento@users.noreply.github.com>
2021-08-11 22:08:46 -0300

    Update 21_08_11_Maven Analytics - Advanced SQL 7.61 - 7.62.sql

--- 21_08_11_Maven Analytics - Advanced SQL 7.61 - 7.62.sql
+++ 21_08_11_Maven Analytics - Advanced SQL 7.61 - 7.62.sql
@@ -8,21 +8,21 @@ USE mavenfuzzyfactory;
 
 -- Lessons 61 - 62: Analyzing Channel Portfolio Trends
 SELECT
-	MIN(DATE(created_at)) AS week_start_date,
+    MIN(DATE(created_at)) AS week_start_date,
     COUNT(DISTINCT CASE WHEN utm_source = 'gsearch' AND device_type = 'desktop' THEN website_session_id ELSE NULL END) AS g_desk_sesions,
-	COUNT(DISTINCT CASE WHEN utm_source = 'bsearch' AND device_type = 'desktop' THEN website_session_id ELSE NULL END) AS b_desk_sesions, 
+    COUNT(DISTINCT CASE WHEN utm_source = 'bsearch' AND device_type = 'desktop' THEN website_session_id ELSE NULL END) AS b_desk_sesions, 
     COUNT(DISTINCT CASE WHEN utm_source = 'bsearch' AND device_type = 'desktop' THEN website_session_id ELSE NULL END)/
     COUNT(DISTINCT CASE WHEN utm_source = 'gsearch' AND device_type = 'desktop' THEN website_session_id ELSE NULL END) AS b_to_g_ratio_desk,
     COUNT(DISTINCT CASE WHEN utm_source = 'gsearch' AND device_type = 'mobile' THEN website_session_id ELSE NULL END) AS g_mobile_sesions,
-	COUNT(DISTINCT CASE WHEN utm_source = 'bsearch' AND device_type = 'mobile' THEN website_session_id ELSE NULL END) AS b_mobile_sesions, 
+    COUNT(DISTINCT CASE WHEN utm_source = 'bsearch' AND device_type = 'mobile' THEN website_session_id ELSE NULL END) AS b_mobile_sesions, 
     COUNT(DISTINCT CASE WHEN utm_source = 'bsearch' AND device_type = 'mobile' THEN website_session_id ELSE NULL END)/
     COUNT(DISTINCT CASE WHEN utm_source = 'gsearch' AND device_type = 'mobile' THEN website_session_id ELSE NULL END) AS b_to_g_ratio_mobile    
 FROM website_sessions
 WHERE
-	created_at BETWEEN '2012-11-05'AND '2012-12-21'
+    created_at BETWEEN '2012-11-05'AND '2012-12-21'
     AND utm_campaign = 'nonbrand'
 GROUP BY
-	YEARWEEK(created_at);
+    YEARWEEK(created_at);
     
 /* Lower bsearch to gsearch ratio in mobile devices. Beyond that, there was a significant drop off in bsearch 
 traffic after the bid down.*/
