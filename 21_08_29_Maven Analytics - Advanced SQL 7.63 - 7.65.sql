034ef63ec161bf5e532d6cad44fac9bbf190d7f6
Renan Pereira Bento <38670752+renanpbento@users.noreply.github.com>
2021-08-29 22:00:21 -0300

    Update 21_08_29_Maven Analytics - Advanced SQL 7.63 - 7.65.sql

--- 21_08_29_Maven Analytics - Advanced SQL 7.63 - 7.65.sql
+++ 21_08_29_Maven Analytics - Advanced SQL 7.63 - 7.65.sql
@@ -13,7 +13,7 @@ brand search sessions by month, and show those sessions as % of paid
 search nonbrand?*/
 
 SELECT
-	YEAR(created_at) AS year,
+    YEAR(created_at) AS year,
     MONTH(created_at) AS month,
     COUNT(DISTINCT CASE WHEN channel_group = 'paid_nonbrand' THEN website_session_id ELSE NULL END) AS nonbrand,
     COUNT(DISTINCT CASE WHEN channel_group = 'paid_brand' THEN website_session_id ELSE NULL END) AS brand,
@@ -24,14 +24,14 @@ SELECT
     COUNT(DISTINCT CASE WHEN channel_group = 'organic_search' THEN website_session_id ELSE NULL END)/COUNT(DISTINCT CASE WHEN channel_group = 'paid_nonbrand' THEN website_session_id ELSE NULL END) * 100 AS '%organic_nonbrand'
 FROM(
 	SELECT
-		website_session_id,
-		created_at,
-		CASE
-			WHEN utm_source IS NULL AND http_referer IN('https://www.gsearch.com', 'https://www.bsearch.com') THEN 'organic_search'
-			WHEN utm_campaign = 'nonbrand' THEN 'paid_nonbrand'
-			WHEN utm_campaign = 'brand' THEN 'paid_brand'
-			WHEN utm_source IS NULL AND http_referer IS NULL THEN 'direct_type_in'
-		END AS channel_group
+	    website_session_id,
+	    created_at,
+	    CASE
+	       WHEN utm_source IS NULL AND http_referer IN('https://www.gsearch.com', 'https://www.bsearch.com') THEN 'organic_search'
+	       WHEN utm_campaign = 'nonbrand' THEN 'paid_nonbrand'
+       	       WHEN utm_campaign = 'brand' THEN 'paid_brand'
+	       WHEN utm_source IS NULL AND http_referer IS NULL THEN 'direct_type_in'
+	       END AS channel_group
 	FROM website_sessions
 	WHERE created_at < '2012-12-23') AS sessions_channel_group
 GROUP BY 
