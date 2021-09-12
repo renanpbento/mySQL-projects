a5a078e4d08fe97521d79933994740d4abf46aec
Renan Pereira Bento <38670752+renanpbento@users.noreply.github.com>
2021-09-12 17:01:19 -0300

    Update 21_09_12_Maven Analytics - Advanced SQL 7.66 - 7.68.sql

--- 21_09_12_Maven Analytics - Advanced SQL 7.66 - 7.68.sql
+++ 21_09_12_Maven Analytics - Advanced SQL 7.66 - 7.68.sql
@@ -13,14 +13,15 @@ weekly volume patterns, to see if we can find any seasonal trends we should plan
 in 2013. If you can pull session and order volume, that would be excellent.*/
 
 SELECT
-	YEAR(ws.created_at) AS 'year',
+    YEAR(ws.created_at) AS 'year',
     WEEK(ws.created_at) AS 'week',
     MIN(DATE(ws.created_at)) AS 'week_start',
     COUNT(DISTINCT ws.website_session_id) AS sessions,
     COUNT(DISTINCT o.order_id) AS orders
 FROM website_sessions ws
-LEFT JOIN orders o ON ws.website_session_id = o.website_session_id
+LEFT JOIN orders o 
+    ON ws.website_session_id = o.website_session_id
 WHERE
-	ws.created_at < '2013-01-01'
+    ws.created_at < '2013-01-01'
 GROUP BY
-	1, 2;
+    1, 2;
