7aff3b821c6b7d08f2e89a98e8c352f82c9c39a6
Renan Pereira Bento <38670752+renanpbento@users.noreply.github.com>
2021-09-18 16:45:47 -0300

    Update 21_09_18_Maven Analytics - Advanced SQL 7.69 - 7.70.sql

--- 21_09_18_Maven Analytics - Advanced SQL 7.69 - 7.70.sql
+++ 21_09_18_Maven Analytics - Advanced SQL 7.69 - 7.70.sql
@@ -13,23 +13,23 @@ our customer experience. Could you analyze the average website session volume, b
 hour and day and bay weekday, so taht we can staff appropriately.*/
 
 SELECT
-	hour,
+    hour,
     ROUND(AVG(CASE WHEN weekday = 0 THEN website_sessions ELSE NULL END), 1) AS monday,
     ROUND(AVG(CASE WHEN weekday = 1 THEN website_sessions ELSE NULL END), 1) AS tuesday,
     ROUND(AVG(CASE WHEN weekday = 2 THEN website_sessions ELSE NULL END), 1) AS wednesday,
     ROUND(AVG(CASE WHEN weekday = 3 THEN website_sessions ELSE NULL END), 1) AS thursday,
     ROUND(AVG(CASE WHEN weekday = 4 THEN website_sessions ELSE NULL END), 1) AS friday,
     ROUND(AVG(CASE WHEN weekday = 5 THEN website_sessions ELSE NULL END), 1) AS saturday,
-	ROUND(AVG(CASE WHEN weekday = 6 THEN website_sessions ELSE NULL END), 1) AS sunday
+    ROUND(AVG(CASE WHEN weekday = 6 THEN website_sessions ELSE NULL END), 1) AS sunday
 FROM (
 	SELECT
-		DATE(created_at) AS created_date,
-		WEEKDAY(created_at)	AS weekday,
-		HOUR(created_at) AS hour,
-		COUNT(DISTINCT website_session_id) AS website_sessions
+	    DATE(created_at) AS created_date,
+	    WEEKDAY(created_at)	AS weekday,
+	    HOUR(created_at) AS hour,
+	    COUNT(DISTINCT website_session_id) AS website_sessions
 	FROM website_sessions
 	WHERE
-		created_at BETWEEN '2012-09-15' AND '2012-11-15'
+	    created_at BETWEEN '2012-09-15' AND '2012-11-15'
 	GROUP BY
 		1, 2, 3
 	) AS daily_hr_sessions
