5fab0fd1415559faa4785a88d5928c93ae07b66e
Renan Pereira Bento <38670752+renanpbento@users.noreply.github.com>
2021-10-13 21:34:19 -0300

    Add files via upload

---
+++ 21_10_13_Maven Analytics - Advanced SQL 7.78 - 7.79.sql
@@ -0,0 +1,20 @@
+-- Maven Analytics - Advanced SQL: MySQL Data Analysis & Business Intelligence
+
+SET GLOBAL max_allowed_packet=16M;
+
+USE mavenfuzzyfactory;
+
+-- Section 9: Product Analysis
+
+-- Lessons 78 - 79: Product_Level Webste Pathing
+
+/* Assignment: Could you please pull clickthrough rates from /products since the new
+product launch on January 6th 2013 by product and compare to the 3 months leading up
+to launch as a baseline*/
+
+SELECT *
+
+FROM products
+;
+	
+
